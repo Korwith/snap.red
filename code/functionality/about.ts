50380d6d6d51deaffa05baee3b2eec6e28653929
Kircic <80077493+Korwith@users.noreply.github.com>
2026-07-25 04:31:47 -0400

    added content to about page

--- code/functionality/about.ts
+++ code/functionality/about.ts
@@ -1,19 +1,31 @@
+// what is the website about? page
 class PageAbout extends Page {
+    title: HTMLElement;
+
     constructor(manager: PageManager) {
         super(manager, 'about');
         this.manager = manager;
         this.element.classList.add('about');
+
+        this.title = document.createElement('h1');
+        this.element.appendChild(this.title);
+
         this.propogateAboutPage();
     }
 
+    // fills about page with content
     private propogateAboutPage(): void {
-        const title: HTMLElement = document.createElement('h1');
-        const description: HTMLElement = document.createElement('span');
-        
-        title.textContent = 'About';
-        description.textContent = 'Under Construction';
+        this.title.textContent = 'Snapshot';
+        this.addDescription('The purpose of this project is a personal replacement to large social media services for the purposes of photo sharing. The project is entirely written in TypeScript, HTML, and CSS. Forever a work in progress.');
+        this.addDescription('This project was written entirely by Thaddeus M.');
+        this.addDescription('This project utilizes the open source JavaScript mapping library, Leaflet.');
+        this.addDescription('&copy; kircic.org');
+    }
 
-        this.element.appendChild(title);
+    // adds a paragraph in the about page
+    private addDescription(text: string): void {
+        const description: HTMLElement = document.createElement('span');
+        description.innerHTML = text;
         this.element.appendChild(description);
     }
 }
