effcfa36d7b8a63349b2d86d393c58c4850834cb
Kircic <80077493+Korwith@users.noreply.github.com>
2026-07-27 16:04:24 -0400

    better generic map type

--- code/maps/maps.ts
+++ code/maps/maps.ts
@@ -13,22 +13,47 @@ class PageMaps extends Page {
 
 // generic type inherited by the main map and under selected photos
 abstract class GenericMap {
+    manager: PageManager;
     element: HTMLElement;
     id: string;
     map: L.Map;
 
-    constructor(element: HTMLElement, id: string) {
+    constructor(manager: PageManager, element: HTMLElement, id: string) {
+        this.manager = manager;
         this.element = element;
         this.id = id;
 
         this.element.setAttribute('id', this.id);
-        this.map = L.map(this.id).setView([39.4123, -77.4255], 13);
+        this.map = L.map(this.element).setView([39.4123, -77.4255], 13); // frederick, may update later
 
         L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
             attribution: '<a href="https://snap.red/">snap.red</a>',
             maxZoom: 18,
             minZoom: 4,
         }).addTo(this.map);
+    }
+
+    createImageMarker(date: string, id: number | string): L.Marker {
+        const user: string = this.manager.fetchUserName();
+        const entry: PhotoEntry | null = this.manager.fetchImageByDate(date);
+        if (!entry) throw new Error('No images found at date');
+        if (!entry.gps) throw new Error('No GPS coordinates found');
+
+        const coordinates: L.LatLngTuple = entry.gps[id] as L.LatLngTuple;
+        const url: string = `../media/${user}/IMG_${id}.jpg`;
+        const icon: L.DivIcon = L.divIcon({
+            className: 'marker',
+            html: `<img src="${url}"></img>`,
+            iconSize: [75, 75],
+            iconAnchor: [75 / 2, 75],
+        });
+
+        return L.marker(coordinates, { icon: icon });
+    }
+
+    // appends the marker directly to this map instance
+    appendImageMarker(marker: L.Marker) {
+        marker.addTo(this.map);
     }
 }
 
@@ -38,7 +63,7 @@ class MainMap extends GenericMap {
     clusters: L.MarkerClusterGroup;
 
     constructor(page: PageMaps) {
-        super(page.element, 'main_map');
+        super(page.manager, page.element, 'main_map');
         this.page = page;
         this.clusters = L.markerClusterGroup();
         this.map.addLayer(this.clusters);
@@ -48,7 +73,6 @@ class MainMap extends GenericMap {
 
     // scans current selected user and plots tagged points on the large map
     // create seperate methods for generic map type later so i can incorporate maps elsewhere
-    // scans current selected user and plots tagged points on the large map
     addMarkers(): void {
         const manager: PageManager = this.page.manager;
         const user: string = manager.fetchUserName();
@@ -61,21 +85,12 @@ class MainMap extends GenericMap {
             if (!entry.gps) continue;
 
             for (const id in entry.gps) {
-                const coordinates: L.LatLngTuple = entry.gps[id] as L.LatLngTuple;
-                const url: string = `../media/${user}/IMG_${id}.jpg`;
-                const icon: L.DivIcon = L.divIcon({
-                    className: 'marker',
-                    html: `<img src="${url}"></img>`,
-                    iconSize: [75, 75],
-                    iconAnchor: [75 / 2, 75],
-                });
-
-                const marker: L.Marker = L.marker(coordinates, { icon: icon });
-                add_markers.push(marker);
+                const image_marker: L.Marker = this.createImageMarker(date, id);
+                add_markers.push(image_marker);
             }
         }
 
-        // Add all markers to the cluster group at once
+        // add all markers to the cluster group at once
         this.clusters.addLayers(add_markers);
     }
 }
