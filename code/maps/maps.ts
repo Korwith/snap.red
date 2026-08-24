b015c6f5820149386a83cfc8d154b7fc469174b7
Kircic <80077493+Korwith@users.noreply.github.com>
2026-08-24 18:54:19 -0400

    css satellite button

--- code/maps/maps.ts
+++ code/maps/maps.ts
@@ -147,6 +147,7 @@ class SatelliteToggleButton {
         this.page = page;
         this.element = document.createElement('button');
         this.element.classList.add('satellite');
+        this.element.textContent = 'Satellite';
         L.DomEvent.disableClickPropagation(this.element);
 
         this.element.onclick = (e: PointerEvent) => this.onclick(e);
