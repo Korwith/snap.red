a8e6a2c7ab352c4dfaa2332ae0f44045b3126d69
Korwith <caseaway01@gmail.com>
2026-09-26 01:56:37 -0400

    new day: gambrill

--- code/data.ts
+++ code/data.ts
@@ -52,6 +52,11 @@ const Data: Database = {
         },
 
         images: {
+            '09/25/26': {
+                name: 'Gambrill State Park',
+                id: [6008, 6019, 6024, 6036, 6038, 6050],
+                camera: CameraConfigApple14
+            },
             '09/16/26': {
                 name: 'Gambrill State Park',
                 id: [5691, 5685, 5645, 5688, 5671, 5696],
